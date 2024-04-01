6e6f48661f5212b0101f676c893fc0336aa53d8c
Mingyuuu0108 <dlalsrb@dgsw.hs.kr>
2024-04-01 16:49:02 +0900

    feat: Add icon file

--- Source/DDS/Foundation/Iconography/Iconography.swift
+++ Source/DDS/Foundation/Iconography/Iconography.swift
@@ -16,6 +16,7 @@ public enum DodamIconography: String {
     case colorfulBus           = "ColorfulBus"
     case colorfulCalender      = "ColorfulCalender"
     case convenienceStore      = "ConvenienceStore"
+    case domomLogo             = "DomomLogo"
     case doorOpen              = "DoorOpen"
     case exclamationmarkCircle = "ExclamationmarkCircle"
     case eye                   = "Eye"
